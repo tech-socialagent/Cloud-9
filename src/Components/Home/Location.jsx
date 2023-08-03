fc98f422dc68b46f9d4744c870a7ce2ddec90e7b
NaveenprasanthT <naveenprasanth2109@gmail.com>
2023-08-03 10:39:42 +0530

    locaion changes

--- src/Components/Home/Location.jsx
+++ src/Components/Home/Location.jsx
@@ -41,7 +41,7 @@ function Location() {
             <SectionHeader title="Location" desc="Location." para="A 5-minute drive from Cloud 9 will take you to the IT hub, Electronic City, Koramangala, JP Nagar, HSR layout and Narayana Hrudayalaya. It is well-connected to educational institutions, shopping centres and MG Road is just 20 km away." />
             <div className={styles.LocationContainer}>
                 <div className={styles.LocationMap}>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.124263659162!2d77.57142287418425!3d13.027757887292763!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae17c4b2ec4fb1%3A0xbd97287df0b88f9e!2sPRASIDDHI%20GROUP!5e0!3m2!1sen!2sin!4v1690890946176!5m2!1sen!2sin" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56205.23698418843!2d77.68268302722723!3d12.804290922696605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae6c3a0f757269%3A0xf8a79d1bdffaf23f!2sCloud9%20Villas%2C%20CLOUD-9%20VILLAS%2C%20Electronic%20City%2C%20Bengaluru%2C%20Bommasandra%2C%20Karnataka%20560099%2C%20India!5e0!3m2!1sen!2suk!4v1691038766301!5m2!1sen!2suk" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div className={styles.LocationRight}>
                     <p>Phone: 099008 47847</p>
