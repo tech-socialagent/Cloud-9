888ff96694b34ba8a9f04aa4882c05f9aa7d96fe
NaveenprasanthT <naveenprasanth2109@gmail.com>
2023-08-05 11:41:23 +0530

    updates 2

--- src/pages/api/zohoapi.js
+++ src/pages/api/zohoapi.js
@@ -66,7 +66,7 @@ export default async function handler(req, res) {
       });
 
     // Return the response to the client
-    return res.status(200).json(sendData.data);
+    return res.status(200).json(response.data);
   } catch (error) {
     // Handle errors
     console.error('Error getting access token:', error.response);
