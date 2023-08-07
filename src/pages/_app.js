ab0001a7d2daff5ebdcc237fd21276044454d132
NaveenprasanthT <naveenprasanth2109@gmail.com>
2023-08-07 17:11:58 +0530

    Google tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,16 +4,12 @@ import Script from 'next/script'
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/ns.html?id=GTM-MLR264PQ"></Script>
-      <Script>
-        {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'GTM-MLR264PQ');
-            `}
-      </Script>
+      <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MLR264PQ');`}}></Script>
       <Component {...pageProps} />
     </>
   )
